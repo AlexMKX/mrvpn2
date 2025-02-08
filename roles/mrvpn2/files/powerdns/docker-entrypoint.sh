743c4716fbfac03223760bb9bb0e0f6e18f8c8a6
Alex MKX <github-alex@xxl.cx>
2025-02-08 18:28:09 +0400

    add doh blocking and some compose fixes

--- roles/mrvpn2/files/powerdns/docker-entrypoint.sh
+++ roles/mrvpn2/files/powerdns/docker-entrypoint.sh
@@ -24,4 +24,19 @@ sudo nft add chain ip $TABLE prerouting '{ type nat hook prerouting priority dst
 sudo nft add rule ip $TABLE prerouting iif \{ "${interfaces[*]}" \} udp dport 53 counter dnat to "$my_ip":5301
 sudo nft add rule ip $TABLE prerouting iif \{ "${interfaces[*]}" \} tcp dport 53 counter dnat to "$my_ip":5301
 
+# Attempt to download the latest DoH blocklist
+curl -s https://raw.githubusercontent.com/dibdot/DoH-IP-blocklists/refs/heads/master/doh-ipv4.txt -o /doh-ipv4-new.txt
+
+# Check if the download was successful and the file is not empty, otherwise use the fallback
+if [ ! -s /doh-ipv4-new.txt ]; then
+    echo "Failed to download or empty file, using fallback..."
+    cp /doh-ipv4.txt /doh-ipv4-new.txt
+fi
+
+# Process the blocklist and apply nftables rules
+sed 's/\s.*//' /doh-ipv4-new.txt | while read ip; do
+    nft add rule ip dns-redir prerouting iif "wg-firezone" ip daddr $ip tcp dport 443 counter drop
+    nft add rule ip dns-redir prerouting iif "wg-tik" ip daddr $ip tcp dport 443 counter drop
+done
+
 /usr/local/sbin/pdns_recursor-startup "$@"
